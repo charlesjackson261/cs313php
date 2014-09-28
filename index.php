f336983146f16e90c89f4e0c64ec16eab33aa88e
jaxntech <cmj.android@gmail.com>
2014-09-27 20:32:14 -0700

    Update to remove the assignment from the page

--- index.php
+++ index.php
@@ -8,28 +8,6 @@
     </head>
     <body>
         Hello World!
-        
-        OBJECTIVE
-The purpose of this assignment is to review a minimal level of client-side programming and provide a homepage from which you can link to future assignments.
-
-GUIDELINES
-Consider the following guidelines as the minimum standard for this assignment:
-
-Professional look and feel
-Do not use an HTML generator/layout program (e.g., DreamWeaver, FrontPage, Visual Studio designer tools). Please write the HTML for this page from scratch. You are, however, welcome to use jQuery, or Bootstrap, or other libraries if you would like.
-Put all styles in an external stylesheet. (No inline styles.)
-Have at least two pages: one as an introduction to yourself and one that will have links to assignments. (For now the assignments page can say “Coming soon” or something similar).
-At least one image
-Something dynamic (e.g., mouse-overs, menus, on-click, on-change, etc.)
-Include reasonable colors, images, etc. to show your creativity and personality (but don’t forget to keep it somewhat professional).
-No run-time errors
-As always, you are encouraged to go above and beyond these guidelines.
-
-After completing these steps, please update the Student Wiki to point to your new homepage (if the URL is different than your “Hello World” page). 
-
-Then, set up a public repository at GitHub, and push your code to it, following the instructions:1.06 Configuring GitHub as a Second Remote. Add an additional link on the wiki to your GitHub repository. Thus, everyone should have two links on the course wiki, one for the actual web page at OpenShift, and one for the source code at GitHub
-
-Finally, click Open and submit the link to your new homepage to the dropbox in I-Learn. 
     </body>
     
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
